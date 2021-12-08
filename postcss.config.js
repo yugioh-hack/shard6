c9e8d6b1e09ef8bb3921bf63c9520bebb0a0ecbf
yugioh-hack <info@yugioh-hack.com>
2021-12-09 00:13:39 +0900

    postcss-eachとpostcss-mixinsを導入した

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
 module.exports = {
     plugins: [
       require('tailwindcss'),
+      require('postcss-mixins'),
       require('autoprefixer'),
+      require('postcss-simple-vars')({ silent: true }),
       require('postcss-import'),
+      require('postcss-each'),
       require('postcss-nested')
     ]
   }
